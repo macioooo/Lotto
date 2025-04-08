e52d44eee0c58bdbd945ffb289f85035836b0934
Maciej Szuwarowski <m9szuwarowski@interia.pl>
2025-04-09 00:03:55 +0200

    added spring boot integration settings

---
+++ src/intergration/java/org.maciooo/BaseIntegrationTest.java
@@ -0,0 +1,55 @@
+package org.maciooo;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@SpringBootTest(classes = LottoSpringBootApplication.class)
+@ActiveProfiles("integration") //reading the file application-integration.yml
+@AutoConfigureMockMvc //throwing http requests
+@Testcontainers //starting db just for tests
+public class BaseIntegrationTest {
+
+    public static final String WIRE_MOCK_HOST = "http://localhost";
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Autowired
+    public ObjectMapper objectMapper;
+    //creating new db
+    @Container
+    public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+
+    //server http
+    @RegisterExtension
+    public static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .build();
+    @DynamicPropertySource
+    public static void propertyOverride(DynamicPropertyRegistry registry) {
+        //setting db address to container
+        registry.add("spring.data.mongodb.uri",mongoDBContainer::getReplicaSetUrl);
+
+    }
+
+    @Test
+    public void f() {
+
+    }
+}
